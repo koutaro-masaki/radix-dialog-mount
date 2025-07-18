47ce7ea10b79297f24ba090814fa4c8e99aac02a
koutaro-masaki <koutaro.masaki@glucose.jp>
2025-07-18 23:43:35 +0900

    swcやめた

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
