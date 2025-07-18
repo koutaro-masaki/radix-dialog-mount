3f3135432d926ddb429358bceafa358bb66df496
koutaro-masaki <koutaro.masaki@glucose.jp>
2025-07-18 23:34:32 +0900

    setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/radix-dialog-mount/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
